17daf98d593d715f57240c5ae444b2ce4e35683d
Risvanth K M <risvanth129@gmail.com>
2026-07-13 01:32:09 +0530

    Optimize health check script for load balancer

--- DevOPS_Task1/verify_load_balancer.sh
+++ DevOPS_Task1/verify_load_balancer.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-for i in {1..10}; do
-    curl -sk https://localhost/healthz
-    echo
-    sleep 1
-done
+seq 1 50 | xargs -n1 -P10 curl -sk https://localhost/healthz
 
 wrk -t4 -c100 -d30s https://localhost/healthz
